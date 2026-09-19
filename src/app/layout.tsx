eb47d2b3d9d74801b74d2b939de8704886a9df6e
RifqiMuhA <rifqiahdan03@gmail.com>
2026-09-19 13:03:06 +0700

    feat: install Maze universal tracking snippet in head of layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       lang="id"
       className="h-full antialiased"
     >
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function (m, a, z, e) { var s, t, u, v; try { t = m.sessionStorage.getItem('maze-us'); } catch (err) {} if (!t) { t = new Date().getTime(); try { m.sessionStorage.setItem('maze-us', t); } catch (err) {} } u = document.currentScript || (function () { var w = document.getElementsByTagName('script'); return w[w.length - 1]; })(); v = u && u.nonce; s = a.createElement('script'); s.src = z + '?apiKey=' + e; s.async = true; if (v) s.setAttribute('nonce', v); a.getElementsByTagName('head')[0].appendChild(s); m.mazeUniversalSnippetApiKey = e; })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', 'c0239654-47e0-4e72-a951-6b00876e09c6');`,
+          }}
+        />
+      </head>
       <body className="min-h-full flex flex-col">
         <AuthProvider>
           <AccessibilityProvider>
